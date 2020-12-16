3f77d088cb5f545a0a44e27f6314de6fe0ad1231
Nidhi Kumari <NIDHI1.KUMARI@lntinfotech.com>
2020-12-16 16:57:56 +0530

    New file change

---
+++ SampleSolution/SampleProject/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProject
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int num, i, count = 0, min, max;
+            Console.WriteLine("Enter the first number");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            max = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("The prime numbers between {0} and {1} are : \n", min, max);
+            for (num = min; num <= max; num++)
+            {
+                count = 0;
+
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+
+                if (count == 0 && num != 1)
+                    Console.Write("{0} ", num);
+                //Console.ReadKey();
+            }
+            Console.WriteLine("\n");
+            Console.ReadKey();
+        }
+    }
+}
